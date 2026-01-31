364dfa3fd8efe5b07716be7f4152450b7251994d
Kaushtubh Tiwari <kaushtubh.2024ug2021@iiitranchi.ac.in>
2026-01-31 08:01:07 +0530

    adding spatial_analysis

--- Backend/spatial_analysis.py
+++ Backend/spatial_analysis.py
@@ -144,4 +144,4 @@ async def http_exception_handler(request, exc: HTTPException):
     )
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=6000)
